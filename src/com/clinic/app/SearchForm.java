9a701c774ee5f19d9e95c3d2734247582631324c
Nakul Kumar <nakul.kumar.in@snapdeal.com>
2015-06-02 23:24:27 +0530

    Search api:form

---
+++ src/com/clinic/app/SearchForm.java
@@ -0,0 +1,51 @@
+package com.clinic.app;
+
+public class SearchForm {
+
+	private String firstname;
+	private String lastname;
+	private String mobile;
+	private String dependent;
+	private String refferedBy;
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getDependent() {
+		return dependent;
+	}
+
+	public void setDependent(String dependent) {
+		this.dependent = dependent;
+	}
+
+	public String getRefferedBy() {
+		return refferedBy;
+	}
+
+	public void setRefferedBy(String refferedBy) {
+		this.refferedBy = refferedBy;
+	}
+
+}
