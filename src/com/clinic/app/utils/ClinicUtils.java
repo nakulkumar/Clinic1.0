b185293361caf2ec958a9e5c207470cd845d4e0c
Nakul Kumar <nakul.kumar.in@snapdeal.com>
2015-06-04 10:30:43 +0530

    Search implemented

---
+++ src/com/clinic/app/utils/ClinicUtils.java
@@ -0,0 +1,14 @@
+package com.clinic.app.utils;
+
+import com.clinic.app.SearchForm;
+
+public class ClinicUtils {
+
+	//Checks if SearchForm is empty or not
+	public static boolean isEmpty(SearchForm search){
+		if (search.getFirstname().isEmpty() && search.getLastname().isEmpty() && search.getMobile().isEmpty() && search.getDependent().isEmpty() && search.getRefferedBy().isEmpty()) {
+			return true;
+		}
+		return false;
+	}
+}
